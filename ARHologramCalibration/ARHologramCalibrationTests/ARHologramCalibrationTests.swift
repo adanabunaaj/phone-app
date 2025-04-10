c588fd890a8b5d7e50a349e35162ce61e8cef216
adanabunaaj <adanabunaaj@gmail.com>
2025-04-10 12:59:48 -0400

    added a hologram

---
+++ ARHologramCalibration/ARHologramCalibrationTests/ARHologramCalibrationTests.swift
@@ -0,0 +1,17 @@
+//
+//  ARHologramCalibrationTests.swift
+//  ARHologramCalibrationTests
+//
+//  Created by Adan Abu Naaj on 4/10/25.
+//
+
+import Testing
+@testable import ARHologramCalibration
+
+struct ARHologramCalibrationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
